9de4b8169ea19d6627ca50e2939b79888c43824e
Shivilnna <pep.poupardin@gmail.com>
2017-01-18 14:30:32 +0100

    Commit Exercice 2.

--- Exercice2/connect.php
+++ Exercice2/connect.php
@@ -3,7 +3,7 @@
 // rectification du dbname qui est exercice_2 dans mon cas.
 // Ajout du try / catch pour la connection a la BDD.
 try {
-    $Db_exo = new PDO("mysql:host=localhost;dbname=exercise_2", "root", "");
+    $db = new PDO("mysql:host=localhost;dbname=exercise_2", "root", "");
 } catch (Exception $e) {
     die($e->getMessage());
 }
